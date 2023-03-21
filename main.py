38315a4e06c05f895d104e754940d713fa70bf25
BillStephens2022 <two4onebill@yahoo.com>
2023-03-21 16:40:51 -0400

    moved number selection to a function

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ print(logo)
 print("Welcome to the Number Guess game!")
 print("I'm thinking of a number between 1 and 100.")
 
-
 def prompt_user_difficulty():
     invalid_answer = True
     number_of_guesses = 0
@@ -23,7 +22,11 @@ def prompt_user_difficulty():
         else:
             print(f"invalid entry, please type 'easy' or 'hard'")
 
+def pick_number():
+    random_number = random.randint(1, 100)
+    return random_number
+
 number_of_guesses = prompt_user_difficulty()
 print(f"you have {number_of_guesses} attempts remaining to the guess the number.")
-random_number = random.randint(1, 100)
-print(random_number)
+selected_number = pick_number()
+print(selected_number)
