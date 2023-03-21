e5bca8937a874c11e982a512cfd7eac37295f31b
BillStephens2022 <two4onebill@yahoo.com>
2023-03-21 16:59:24 -0400

    prompt user to make guess, check guess, and return message to user

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import random
 from art import logo
 
-print(logo)
-print("Welcome to the Number Guess game!")
-print("I'm thinking of a number between 1 and 100.")
-
 def prompt_user_difficulty():
     invalid_answer = True
     number_of_guesses = 0
@@ -26,7 +22,39 @@ def pick_number():
     random_number = random.randint(1, 100)
     return random_number
 
-number_of_guesses = prompt_user_difficulty()
-print(f"you have {number_of_guesses} attempts remaining to the guess the number.")
-selected_number = pick_number()
-print(selected_number)
+def prompt_guess():
+    user_guess = int(input("Make a guess: "))
+    return user_guess
+
+def check_guess(guess, selected_number, remaining_guesses):
+    if guess == selected_number:
+        remaining_guesses = 0
+        message = "You win"
+        return message
+    elif guess > selected_number:
+        remaining_guesses -= 1
+        message = "Too High.\nGuess again."
+        return message
+    else:
+        remaining_guesses -= 1
+        message = "Too Low.\nGuess again."
+        return message
+
+should_continue = True
+
+print(logo)
+print("Welcome to the Number Guess game!")
+
+while should_continue:
+    print("I'm thinking of a number between 1 and 100.")
+    remaining_guesses = prompt_user_difficulty()
+    print(f"you have {remaining_guesses} attempts remaining to the guess the number.")
+    selected_number = pick_number()
+    print(selected_number)
+    user_guess = prompt_guess()
+    message = check_guess(user_guess, selected_number, remaining_guesses)
+    print(message)
+    if remaining_guesses == 0:
+        print(message)
+        should_continue = False
+    
