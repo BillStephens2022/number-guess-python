78fc7fa9cd17d987e5ae3e4e033dcd416301c3db
BillStephens2022 <two4onebill@yahoo.com>
2023-03-21 17:27:53 -0400

    reduce count with each guess and exit loop once user wins or runs out of guesses

--- main.py
+++ main.py
@@ -26,35 +26,39 @@ def prompt_guess():
     user_guess = int(input("Make a guess: "))
     return user_guess
 
-def check_guess(guess, selected_number, remaining_guesses):
+def check_guess(guess, selected_number):
     if guess == selected_number:
-        remaining_guesses = 0
-        message = "You win"
-        return message
+        return "You win"
     elif guess > selected_number:
-        remaining_guesses -= 1
-        message = "Too High.\nGuess again."
-        return message
+        return "Too High.\nGuess again."
     else:
-        remaining_guesses -= 1
-        message = "Too Low.\nGuess again."
-        return message
+        return "Too Low.\nGuess again."
+        
 
 should_continue = True
 
 print(logo)
 print("Welcome to the Number Guess game!")
+print("I'm thinking of a number between 1 and 100.")
 
-while should_continue:
-    print("I'm thinking of a number between 1 and 100.")
+while should_continue == True:
     remaining_guesses = prompt_user_difficulty()
     print(f"you have {remaining_guesses} attempts remaining to the guess the number.")
     selected_number = pick_number()
     print(selected_number)
-    user_guess = prompt_guess()
-    message = check_guess(user_guess, selected_number, remaining_guesses)
-    print(message)
-    if remaining_guesses == 0:
+    while remaining_guesses > 0:
+        user_guess = prompt_guess()
+        message = check_guess(user_guess, selected_number)
         print(message)
-        should_continue = False
+        if message == "You win":
+            print("BING BING. Thanks for playing")
+            should_continue = False
+            remaining_guesses = 0
+        else:
+            remaining_guesses -= 1
+            print(f"You have {remaining_guesses} attempts remaining to guess the number.")
+    print("Game Over")
+    should_continue = False
+            
+   
     
