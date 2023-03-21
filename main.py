4036c9fe2ad3dfb1acecb931ab367fe4baa01867
BillStephens2022 <two4onebill@yahoo.com>
2023-03-21 17:39:42 -0400

    prompt user to play again and remove print statement showing computer's selection

--- main.py
+++ main.py
@@ -28,37 +28,45 @@ def prompt_guess():
 
 def check_guess(guess, selected_number):
     if guess == selected_number:
-        return "You win"
+        return "You win!!"
     elif guess > selected_number:
         return "Too High.\nGuess again."
     else:
         return "Too Low.\nGuess again."
         
 
-should_continue = True
+
 
 print(logo)
 print("Welcome to the Number Guess game!")
 print("I'm thinking of a number between 1 and 100.")
 
-while should_continue == True:
-    remaining_guesses = prompt_user_difficulty()
-    print(f"you have {remaining_guesses} attempts remaining to the guess the number.")
-    selected_number = pick_number()
-    print(selected_number)
-    while remaining_guesses > 0:
-        user_guess = prompt_guess()
-        message = check_guess(user_guess, selected_number)
-        print(message)
-        if message == "You win":
-            print("BING BING. Thanks for playing")
-            should_continue = False
-            remaining_guesses = 0
-        else:
-            remaining_guesses -= 1
-            print(f"You have {remaining_guesses} attempts remaining to guess the number.")
-    print("Game Over")
-    should_continue = False
+def play_game(): 
+    should_continue = True
+    while should_continue:
+        remaining_guesses = prompt_user_difficulty()
+        print(f"you have {remaining_guesses} attempts remaining to the guess the number.")
+        selected_number = pick_number()
+        while remaining_guesses > 0:
+            user_guess = prompt_guess()
+            message = check_guess(user_guess, selected_number)
+            print(message)
+            if message == "You win!!":
+                print("Thanks for playing!")
+                should_continue = False
+                remaining_guesses = 0
+            else:
+                remaining_guesses -= 1
+                print(f"You have {remaining_guesses} attempts remaining to guess the number.")
+        print("Game Over")
+        should_continue = False
+
+play_game()
+play_again = input("Would you like to play again? Type 'y' or 'n': ")
+if play_again == "y":
+    play_game()
+else:
+    print("Thanks for playing, come back soon!")
             
    
     
